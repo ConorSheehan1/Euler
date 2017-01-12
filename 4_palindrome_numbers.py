e96a4276158371dfce66c86e5eead692a0ae74ca
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2017-01-12 14:13:04 +0000

    4 solved in python

--- 4_palindrome_numbers.py
+++ 4_palindrome_numbers.py
@@ -2,12 +2,14 @@ def is_pal(n):
     s = str(n)
     return s == s[::-1]
 
-a = 100
-b = 100
+pals = []
+for a in range(999, 100, -1):
+    for b in range(999, 100, -1):
+        # print(a, b)
+        product = a*b
+        if is_pal(product):
+            pals.append(product)
+print(max(pals))
 
-while True:
-    product = a*b
-    if is_pal(product):
-        print(product)
 
 
