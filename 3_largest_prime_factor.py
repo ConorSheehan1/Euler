1c64864a887089d7a43e5db76967e36a65a15a01
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2017-01-12 13:12:19 +0000

    3 solved, find factors first, then check if prime

--- 3_largest_prime_factor.py
+++ 3_largest_prime_factor.py
@@ -1,34 +1,17 @@
-# def isPrime(n):
-#     return not any(n % i == 0 for i in range(2, n//2 + 1))
+def isPrime(n):
+    return not any(n % i == 0 for i in range(2, n//2 + 1))
 
 
-# def isPrime(n):
-#     for i in range(2, n//2 + 1):
-#         if n % i == 0:
-#             return False
-#     return True
-
-# for i in range(2, 600851475143//2 + 1):
-#     current = 0
-#     if 600851475143 % i == 0 and isPrime(i):
-#         print(i)
-#         current = i
-# print(current)
-
-prime_list = [2]
-for i in range(2, 600851475143//2 + 1):
-    current = 0
-    if 600851475143 % i == 0:
-        if all(i % val != 0 for val in prime_list):
-            prime_list.append(i)
-            print(i)
-            current = i
-print(current)
+# get factors, then find the largest one that's a prime
+factors = [i for i in range(2, int(600851475143**0.5) + 1) if 600851475143%i==0]
+print(factors)
+for val in factors[::-1]:
+    if isPrime(val):
+        print(val)
+        break
 
 
-# def isPrime(n):
-#     if all(n % i != 0 for i in prime_list):
-#         prime_list.append(i)
+
 
 
 
