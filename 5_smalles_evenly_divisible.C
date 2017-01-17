b9f813d04ebcf8267a3121a203e2a52768cdacd5
Conor Sheehan <conor.sheehan.2@ucdconnect.ie>
2017-01-17 22:21:54 +0000

    5 solved in C

---
+++ 5_smalles_evenly_divisible.C
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int divis(int n, int limit){
+	/* only need to check from half the size of the number of divisors
+	because they are mirrored.
+	e.g.  if value is divisble by 8, no need to check if it's divisible by 4 or 2. */
+	for(int i = limit/2; i <= limit; i++){
+		if(n % i != 0){
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int main(){
+	int done = 0;
+	int start = 20;
+	int current = start;
+	do{
+		/*printf("%d\n", current);*/
+		if (divis(current, start)==1){
+			printf("%d", current);
+			break;
+		}
+		current++; 		
+		
+	}while(done==0);
+}
--- 5_smalles_evenly_divisible.py
+++ 5_smalles_evenly_divisible.py
@@ -1,9 +1,11 @@
 def min_divisible(top):
     start = top
     while True:
-        if all(start % i == 0 for i in range(2, top)):
-            print(start)
-            break
-        start += 1
+        if any(start % i != 0 for i in range(2, top)):
+            start += 1
+            continue
+        print(start)
+        break
 
-min_divisible(10)
+
+min_divisible(20)
