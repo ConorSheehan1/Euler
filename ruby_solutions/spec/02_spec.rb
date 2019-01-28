93b08e074df5246819aa0561fd5270f64b35691b
conor <conor.sheehan@live.ie>
2019-01-28 16:50:20 +0000

    day 2 update rubocop to match https://gist.github.com/ConorSheehan1/ed75920ca8722a09b527b37f006cf8e3

---
+++ ruby_solutions/spec/02_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "../src/02.rb"
+
+describe "day_02" do
+  it "should do something" do
+
+  end
+end
---
+++ ruby_solutions/src/02.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+def day_02
+
+end
+
+def solution
+
+end
